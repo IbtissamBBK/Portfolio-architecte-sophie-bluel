aefe22d281fd3ce66585e8f5efb4a562586d4097
Ibtissam <benbourekibtissam@gmail.com>
2024-10-27 00:12:30 +0200

    added comments login form

--- FrontEnd/js/login.js
+++ FrontEnd/js/login.js
@@ -1,3 +1,5 @@
+
+// Sélection du form avec l'id et ajout d'un événement pour la soumission du formulaire
 document.getElementById('login-form').addEventListener('submit', async function(event) {
                          
 event.preventDefault(); // Désactivation du comportement par défaut du navigateur
@@ -5,22 +7,24 @@ event.preventDefault(); // Désactivation du comportement par défaut du navigat
     const email = document.getElementById('email').value;
     const password = document.getElementById('password').value;
 
+    // Envoie une requête POST à l'API pour tenter de se connecter avec les identifiants fournis
     const response = await fetch('http://localhost:5678/api/users/login', {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ email, password })
+        body: JSON.stringify({ email, password }) // Convertit l'email et le mot de passe en chaîne JSON avant l'envoi
     });
 
+    // Attend la réponse JSON de l'API (qui contient le token ou une erreur)
     const result = await response.json();
 
     if (response.ok) {
-        // Stocker le token et rediriger vers la page d'accueil
-        localStorage.setItem('authToken', result.token);
+        
+        localStorage.setItem('authToken', result.token); // Si la connexion est réussie, stocke le token d'authentification dans le localStorage du navigateur
         window.location.href = 'index.html'; // Redirection vers la page d'accueil
     } else {
-        // Affiche le  message d'erreur
-        document.getElementById('error-message').style.display = 'block';
+        
+        document.getElementById('error-message').style.display = 'block';// Si connexion échoue cela affiche le  message d'erreur
     }
 });
